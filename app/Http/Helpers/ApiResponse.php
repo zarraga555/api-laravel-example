10c7b08d342dd56fd354ffd2fc2d018d6ff645fe
Alberto Zarraga <zarraga555@gmail.com>
2024-09-28 10:31:49 -0400

    Helper para las respuestas de la API REST

---
+++ app/Http/Helpers/ApiResponse.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Helpers;
+
+class ApiResponse
+{
+    public static function success($data, $message = "Success", $code = 200): \Illuminate\Http\JsonResponse
+    {
+        return response()->json([
+            'status' => 'success',
+            'message' => $message,
+            'data' => $data
+        ], $code);
+    }
+
+    public static function error($message, $errors = null, $code = 400): \Illuminate\Http\JsonResponse
+    {
+        return response()->json([
+            'status' => 'error',
+            'message' => $message,
+            'errors' => $errors
+        ], $code);
+    }
+}
