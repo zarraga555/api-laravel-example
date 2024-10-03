c39eb4adb00b65a66e7f4c483a071e41b2d02248
Alberto Zarraga <zarraga555@gmail.com>
2024-10-02 23:47:14 -0400

    Se agrego migracion y modelo para Customer(Cliente)

---
+++ database/migrations/2024_09_24_161054_create_customers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('identification_number');
+            $table->string('razon_social_bol');
+            $table->string('name');
+            $table->string('lastname');
+            $table->foreignId('created_by')->nullable()->constrained('users');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customers');
+    }
+};
